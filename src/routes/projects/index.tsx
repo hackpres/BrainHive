91d9477fab069d6d766a0d68efbe0e8abbb8ccdc
Hacker <alex.hacker@braintrustcreative.com>
2024-06-18 13:19:07 -0400

    added environment variables for secure API keys/userId's

--- src/routes/projects/index.tsx
+++ src/routes/projects/index.tsx
@@ -3,36 +3,38 @@ import {
     useSignal,
     useStylesScoped$,
     useTask$,
-  } from '@builder.io/qwik';
-  import {
+} from '@builder.io/qwik';
+import {
     routeLoader$,
     Form,
     routeAction$,
     server$,
-  } from '@builder.io/qwik-city';
-  import styles from './index.css?inline';
+} from '@builder.io/qwik-city';
+import styles from './index.css?inline';
 
-  let workspaceCode = "";
-   
-  export const useHiveWorkspace = routeLoader$(async () => {
+let workspaceCode = "";
+const KEY = import.meta.env.PUBLIC_API_KEY;
+const UserID = import.meta.env.PUBLIC_API_USERID;
+
+export const useHiveWorkspace = routeLoader$(async () => {
     const options = {
         method: 'GET',
-        headers: {accept: 'application/json', api_key: '5d4a3e646f3f48f535900fbabe9771a1'}
-      };
-      await fetch('https://app.hive.com/api/v1/workspaces?user_id=WCQGj45v87NQCC9hH', options)
+        headers: { accept: 'application/json', api_key: `${KEY}` }
+    };
+    await fetch(`https://app.hive.com/api/v1/workspaces?user_id=${UserID}`, options)
         .then(response => response.json())
         .then(res => workspaceCode = res[0].id)
         .catch(err => console.error(err));
-    });
-   
-  export default component$(() => {
+});
+
+export default component$(() => {
     useStylesScoped$(styles);
     useHiveWorkspace();
     console.log(workspaceCode);
     return (
-      <section class="section bright">
-        <p>{workspaceCode}</p>
+        <section class="section bright">
+            <p>{workspaceCode}</p>
 
-      </section>
+        </section>
     );
-  });
+});
